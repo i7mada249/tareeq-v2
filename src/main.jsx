de5cc0bba7597aa794cb22ff17b1ef7ebc6f7131
i7mada249 <i7mada249@outlook.com>
2023-04-02 19:38:54 -0700

    hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import {
+  createHashRouter,
+  RouterProvider
+} from 'react-router-dom';
 import App from './App'
 import './index.css'
 
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <React.StrictMode>
-      <App />
+      <RouterProvider router={router} />
     </React.StrictMode>
-  </BrowserRouter>
-
-
 )
