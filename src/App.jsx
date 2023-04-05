f924be40dd6ca19097a2dd45daf75d66822acfe2
i7mada249 <i7mada249@outlook.com>
2023-04-04 17:33:43 -0700

    Scroll restoration

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Nav from './Components/Nav'
-import Header from './Containers/Header/Header'
-import ByRole from './Containers/Main/ByRole'
-import BySkill from './Containers/Main/BySkill'
 import Landingpage from './Landingpage'
 import Frontend from './Pages/Frontend'
 
@@ -15,9 +12,9 @@ function App() {
         <Nav />
       </div>
       <Routes>
-        <Route path='/' exact Component={Landingpage}/>
-        <Route path='/main/frontend' Component={Frontend}/>
-        <Route path='/tareeq-v2' exact component={Landingpage}/>
+          <Route path='/' exact Component={Landingpage} />
+          <Route path='/main/frontend' Component={Frontend} />
+          <Route path='/tareeq-v2' exact component={Landingpage} />
       </Routes>
 
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { HashRouter } from 'react-router-dom';
+import ScrollToTop from '../ScrollToTop';
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
+      <ScrollToTop />
       <App />
     </HashRouter>
   </React.StrictMode>
